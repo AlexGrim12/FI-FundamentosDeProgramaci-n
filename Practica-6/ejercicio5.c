87e7fa58a4268dd5de721fc19a2d4351430b0409
AlexGrim <98252143+AlexGrim12@users.noreply.github.com>
2022-10-10 16:58:49 -0500

    Practica 6 - Terminada

--- Practica-6/ejercicio5.c
+++ Practica-6/ejercicio5.c
@@ -2,17 +2,18 @@
 const int M = 6;
 int main()
 {
-
     int a, b, c;
     puts("Introduce el valor de a y de b");
-    scanf("*%d %d", &a, &b);
+    scanf("%d%d", &a, &b);
     c = 2 * a - b;
     printf("\n c= %d\n", c);
     c = -M;
     printf("\n c=%d\n", c);
     b = a + c - M;
+    printf("\n b=%d\n", b);
     a = b * M;
+    printf("\n a=%d\n", a);
     b = -1;
-    printf(" b= %6d C- %6d \n", b, c);
+    printf(" b= %6d c= %6d \n", b, c);
     return 0;
 }
