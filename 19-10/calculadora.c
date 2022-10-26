68487bc43a82b64f5038bd6eca80c65087319474
AlexGrim <98252143+AlexGrim12@users.noreply.github.com>
2022-10-26 14:27:58 -0500

    correcion de calculadora

--- 19-10/calculadora.c
+++ 19-10/calculadora.c
@@ -3,75 +3,83 @@ int main()
 {
     int a, op, r;
     float n1, n2, n3;
-{
+
 inicio:
-}
-    printf("(1) Suma");
-    printf("(2) Resta");
-    printf("(3) Multiplicaci�n");
-    printf("(4) Divisi�n");
+    printf("(1) Suma\n");
+    printf("(2) Resta\n");
+    printf("(3) Multiplicaci%cn\n", 162);
+    printf("(4) Divisi%cn\n", 162);
+    printf("\nSeleccione una opci%cn -> ", 162);
     scanf("%i", &a);
     switch (a)
     {
     case 1:
-        printf("Dame valores: ");
-        scanf("%f %f", n1, n2);
-        n3 == n1 + n2;
-        printf("%f + %f es igual a %f", n1, n2, n3);
+        printf("Dame el valor 1: ");
+        scanf("%f", &n1);
+        printf("Dame el valor 2: ");
+        scanf("%f", &n2);
+        n3 = n1 + n2;
+        printf("\n%.2f + %.2f es igual a %.2f", n1, n2, n3);
         break;
     case 2:
-        printf("Dame valores: ");
-        scanf("%f %f", n1, n2);
-        n3 == n1 - n2;
-        printf("%f - %f es igual a %f", n1, n2, n3);
+        printf("Dame el valor 1: ");
+        scanf("%f", &n1);
+        printf("Dame el valor 2: ");
+        scanf("%f", &n2);
+        n3 = n1 - n2;
+        printf("\n%.2f - %.2f es igual a %.2f", n1, n2, n3);
         break;
     case 3:
-        printf("Dame valores: ");
-        scanf("%f %f", n1, n2);
-        n3 == n1 *n2;
-        printf("%f X %f es igual a %f", n1, n2, n3);
+        printf("Dame el valor 1: ");
+        scanf("%f", &n1);
+        printf("Dame el valor 2: ");
+        scanf("%f", &n2);
+        n3 = n1 * n2;
+        printf("\n%.2f X %f es igual a %.2f", n1, n2, n3);
         break;
     case 4:
-        printf("Dame valores: ");
-        scanf("%f %f", n1, n2);
+        printf("Dame el valor 1: ");
+        scanf("%f", &n1);
+        printf("Dame el valor 2: ");
+        scanf("%f", &n2);
         printf("(1) n1 / n2");
         printf("(2) n2 / n1");
-        scanf("%i", op);
+        scanf("%i", &op);
         if (op == 1)
         {
             if (n2 == 0)
             {
-                printf("No se puede dividir entre cero");
+                printf("\nNo se puede dividir entre cero");
             }
             else
             {
-                n3 == n1 / n2;
-                printf("%f / %f es igual a %f", n1, n2, n3);
+                n3 = n1 / n2;
+                printf("\n%.2f / %.2f es igual a %.2f", n1, n2, n3);
+            }
+        }
+        else
+        {
+            if (n1 == 0)
+            {
+                printf("\nNo se puede dividir entre cero");
             }
             else
             {
-                if (n1 == 0)
-                {
-                    printf("No se puede dividir entre cero");
-                }
-                else
-                {
-                    n3 == n2 / n1;
-                    printf("%f / %f es igual a %f", n2, n1, n3);
-                }
+                n3 = n2 / n1;
+                printf("\n%.2f / %.2f es igual a %.2f", n2, n1, n3);
             }
         }
     default:
-        printf("Valor no reconocido");
+        printf("\nValor no reconocido");
         break;
     }
-    printf("Quiere repetir el programa? s/n \n");
-    scanf("%i", r);
+    printf("\n\n%cQuiere repetir el programa? \n\nS%c = 1 No = 2 -> ", 168, 161);
+    scanf("%i", &r);
+    printf("\n");
     if (r == 1)
-
     {
-        goto(inicio);
+        goto inicio;
     }
-
+    printf("Saliendo del programa...");
     return 0;
 }
