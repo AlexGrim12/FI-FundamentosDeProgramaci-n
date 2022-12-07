dc5a8c177b26e71ecf84d89091d102c2cd2ff815
Alejandro Grimaldo <98252143+AlexGrim12@users.noreply.github.com>
2022-12-07 18:37:32 +0000

    07-12

--- 07-12/ejercicio1.c
+++ 07-12/ejercicio1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 struct datosAlumno
 {
     char nombre[20];
@@ -10,32 +11,42 @@ struct datosAlumno
     float peso;
 };
 
-struct datosAlumno estudiante;
+struct datosAlumno estudiante[20];
 
-int main(int argc, char const *argv[])
+int main()
 {
+    int n;
     FILE *apFile;
     apFile = fopen("DatosAlumno.txt", "a");
-    printf("Da nombre Alumno: ");
-    gets(estudiante.nombre);
-    printf("Da apellido Alumno: ");
-    gets(estudiante.apellido);
-    printf("Da carrera Alumno: ");
-    gets(estudiante.carrera);
-    printf("Da no de cuenta Alumno: ");
-    scanf("%s", estudiante.noDeCuenta);
-    printf("Da edad Alumno: ");
-    scanf("%d", &estudiante.edad);
-    printf("Da estatura Alumno: ");
-    scanf("%f", &estudiante.estatura);
-    printf("Da peso Alumno: ");
-    scanf("%f", &estudiante.peso);
+    printf("Da la cantidad de alumnos: ");
+    scanf("%i", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Da nombre Alumno: ");
+        gets(estudiante[i].nombre);
+        printf("Da apellido Alumno: ");
+        gets(estudiante[i].apellido);
+        printf("Da carrera Alumno: ");
+        gets(estudiante[i].carrera);
+        printf("Da no de cuenta Alumno: ");
+        scanf("%s", estudiante[i].noDeCuenta);
+        printf("Da edad Alumno: ");
+        scanf("%d", &estudiante[i].edad);
+        printf("Da estatura Alumno: ");
+        scanf("%f", &estudiante[i].estatura);
+        printf("Da peso Alumno: ");
+        scanf("%f", &estudiante[i].peso);
+    }
 
-    printf("\nDatos del alumno: ");
-    printf("\nNombre: %s \nAperllido: %s \nCarrera: %s \nNo de cuenta: %s \nEdad: %d \nEstatura: %.2f \nPeso: %.2f\n", estudiante.nombre, estudiante.apellido, estudiante.carrera, estudiante.noDeCuenta, estudiante.edad, estudiante.estatura, estudiante.peso);
-    //Imprime en el archivo solicitado :)
-    fprintf(apFile, "Nombre: %s \nAperllido: %s \nCarrera: %s \nNo de cuenta: %s \nEdad: %d \nEstatura: %.2f \nPeso: %.2f\n\n", estudiante.nombre, estudiante.apellido, estudiante.carrera, estudiante.noDeCuenta, estudiante.edad, estudiante.estatura, estudiante.peso);
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nDatos del alumno %i: ", i + 1);
+        printf("\nNombre: %s \nAperllido: %s \nCarrera: %s \nNo de cuenta: %s \nEdad: %d \nEstatura: %.2f \nPeso: %.2f\n", estudiante[i].nombre, estudiante[i].apellido, estudiante[i].carrera, estudiante[i].noDeCuenta, estudiante[i].edad, estudiante[i].estatura, estudiante[i].peso);
+
+        //Imprime en el archivo solicitado :)
+        fprintf(apFile, "Datos del alumno %i: ", i + 1);
+        fprintf(apFile, "\nNombre: %s \nAperllido: %s \nCarrera: %s \nNo de cuenta: %s \nEdad: %d \nEstatura: %.2f \nPeso: %.2f\n\n", estudiante[i].nombre, estudiante[i].apellido, estudiante[i].carrera, estudiante[i].noDeCuenta, estudiante[i].edad, estudiante[i].estatura, estudiante[i].peso);
+    }
     fclose(apFile);
-
     return 0;
 }
